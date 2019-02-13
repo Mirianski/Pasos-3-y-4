739f80d2d2c203d02cce47bbcff84f5e1c05fe67
Mirianski <47190614+Mirianski@users.noreply.github.com>
2019-02-13 10:36:24 +0000

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,42 @@
+
+package ut4pruebasintesismiriam;
+
+
+public class Calculadora {
+
+
+    public int getOperador1() {
+        return operador1;
+    }
+
+
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+
+    public int getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    public int getOperacion() {
+        return operacion;
+    }
+
+
+    public void setOperacion(int operacion) {
+        this.operacion = operacion;
+    }
+private int operador1;
+private int operador2;
+private int operacion;
+   
+    public static void main(String[] args) {
+     
+    }
+    
+}
